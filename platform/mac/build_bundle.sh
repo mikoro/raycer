f61768dd1fc419fa462f4a9fe43a8e21156f00a5
Mikko Ronkainen <mikko@mikkoronkainen.com>
2015-05-16 05:46:35 -0700

    Added mac bundle building script

---
+++ platform/mac/build_bundle.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf Raycer.app
+
+mkdir -p Raycer.app/Contents/MacOS
+mkdir -p Raycer.app/Contents/Resources
+mkdir -p Raycer.app/Contents/Frameworks
+
+cp platform/mac/Info.plist Raycer.app/Contents
+cp misc/icons/raycer.icns Raycer.app/Contents/Resources
+cp -R bin/* Raycer.app/Contents/MacOS
+platform/mac/dylibbundler -od -b -x ./Raycer.app/Contents/MacOS/raycer -d ./Raycer.app/Contents/Frameworks/ -p @executable_path/../Frameworks/
