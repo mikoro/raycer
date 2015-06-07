9c05fdb0e3d6a76d2f94b707c3eb76015c6b73f6
Mikko Ronkainen <mikko@mikkoronkainen.com>
2015-06-07 17:37:18 +0300

    Fixed file encodings

--- src/Tests/SamplerTest.cpp
+++ src/Tests/SamplerTest.cpp
@@ -1,4 +1,4 @@
-// Copyright © 2015 Mikko Ronkainen <firstname@mikkoronkainen.com>
+// Copyright Â© 2015 Mikko Ronkainen <firstname@mikkoronkainen.com>
 // License: MIT, see the LICENSE file.
 
 #include <random>
