4034714d947e8d2f2b079804f197ce63d5dab3ce
Mikko Ronkainen <mikko@mikkoronkainen.com>
2015-06-07 17:59:43 +0300

    Fixed compilation error

--- src/Raytracing/Primitives/Mesh.cpp
+++ src/Raytracing/Primitives/Mesh.cpp
@@ -1,6 +1,8 @@
 // Copyright © 2015 Mikko Ronkainen <firstname@mikkoronkainen.com>
 // License: MIT, see the LICENSE file.
 
+#include <exception>
+
 #include "Raytracing/Primitives/Mesh.h"
 #include "Raytracing/Ray.h"
 #include "Utils/ObjReader.h"
