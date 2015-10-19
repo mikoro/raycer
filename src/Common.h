4d34dd1bfdf56119c5caa36ce4805dc74db1e826
Mikko Ronkainen <mikko@mikkoronkainen.com>
2015-10-19 17:06:51 +0300

    Compilation fixes

--- src/Common.h
+++ src/Common.h
@@ -1,4 +1,7 @@
 #pragma once
 
-constexpr auto RAYCER_VERSION = "1.0.0";
-constexpr auto CACHE_LINE_SIZE = 64;
+//constexpr auto RAYCER_VERSION = "1.0.0";
+//constexpr auto CACHE_LINE_SIZE = 64;
+
+#define RAYCER_VERSION "1.0.0"
+#define CACHE_LINE_SIZE 64
